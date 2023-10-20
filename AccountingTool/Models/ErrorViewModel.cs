5b9fec0e7d9ba48de6fe236254ac343915d8d6a0
SeanChen <chenshaoen01@gmail.com>
2023-10-20 23:56:22 +0800

    create project

---
+++ AccountingTool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AccountingTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
